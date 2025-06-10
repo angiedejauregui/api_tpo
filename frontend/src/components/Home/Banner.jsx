299cb8340fe5dc3bd02cb8c13e579c7895163578
agusgiorgetta <agusgiorgetta0944@gmail.com>
2025-06-10 15:24:06 -0300

    Agregamos foto y buscador al banner

--- frontend/src/components/Home/Banner.jsx
+++ frontend/src/components/Home/Banner.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import './Banner.css';
 
 export default function Banner () {
     return(
-        <section className="bannerBanner">
-            <form action="" method="get">
-                <fieldset>
-                    <input type="text" name="Search" id="Search" />
-                </fieldset>
-            </form>
+        <section className="banner">
+            <img src="/pesasGym.png" alt="Banner pesas de gimnasio" className="banner-img"/>
+            <div className="banner-overlay"></div>
+            <div className="banner-content">
+                <form className="search-form" action="" method="get">
+                    <fieldset>
+                        <div className="search-container">
+                            <span className="search-icon">🔍</span>
+                            <input
+                            type="text"
+                            name="Search"
+                            id="Search"
+                            placeholder="Buscar"
+                            />
+                            <span className="filter-icon">⚙️</span>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </section>
     );
 }
--- frontend/src/components/Home/Main.jsx
+++ frontend/src/components/Home/Main.jsx
@@ -5,9 +5,8 @@ export default function Main() {
 
     return(
         <>
-            
-            <h1>Hola</h1>
             <Banner />
+            <h1>Hola</h1>
         </>
     );
 }
